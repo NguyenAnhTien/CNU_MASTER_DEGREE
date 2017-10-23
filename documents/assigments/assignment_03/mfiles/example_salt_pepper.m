7f97110603f76c7ccc36eab524a3035e3ef1620f
TienNguyen <tien.nguyenanh94@gmail.com>
2017-10-23 10:14:19 +0900

    TienNguyen: update assignment 03. Salt and pepper noise.

--- documents/assigments/assignment_03/mfiles/example_salt_pepper.m
+++ documents/assigments/assignment_03/mfiles/example_salt_pepper.m
@@ -2,26 +2,23 @@
 % Just provide the path of the image to add the noise or to restore the
 % image.
 clc;
-f = imread('');
+f = imread('../figures/assignment_03_fig_06.tif');
 figure
 imshow(f),title('Original Image')
 [M N] = size(f);
-f = im2double(f);
 % Any type of noise can b added to the image provided. Just see the
 % imnoise2 file to see the various noise effects. Here an example is shown
 % where we are adding the pepper noise to the image.
-r = imnoise2('gaussian',M,N,0,0.1);
+r = imnoise2('salt & pepper',M,N,0,0.1);
 figure
 imshow(r),title('Noise to be added');
 c = find(r == 1);
 gp = f;
-% gp(c) = 255;
-gp = f + r;
+gp(c) = 255;
 figure
 imshow(gp),title('Image after adding the Noise');
 figure
 histogram(gp);
-% histogram(gp);
 % The image distorted by adding the noise can be restored by using the
 % function imrest. Here an example is shown by using contraharmonic filter.
 % you can use various type of other filter to restore the image. For more
