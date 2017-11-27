ecaafc13a0381d8c8131c36631b12e5329a5a279
tiennguyen <tien.nguyenanh94@gmail.com>
2017-11-27 11:13:21 +0900

    TienNguyen: Update assignment 05

--- documents/assigments/assignment_05/mfiles/exercise_reconstruction.m
+++ documents/assigments/assignment_05/mfiles/exercise_reconstruction.m
@@ -1,4 +1,8 @@
 f = imread('../figures/exercise_reconstruction.tif');
 fe = imerode(f, ones(51, 1));
-fo = imopen(f , ones(51, 1));
+fo = imopen(f, ones(51, 1));
 fobr = imreconstruct(fe, f);
+figure; imshow(f);
+figure; imshow(fe);
+figure; imshow(fo);
+figure; imshow(fobr);
