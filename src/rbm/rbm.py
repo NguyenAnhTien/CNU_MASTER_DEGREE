ddcfc58460babed21ecbf90e66281f01d20e8bdc
TienNguyen <tien.nguyenanh94@gmail.com>
2018-05-28 11:30:04 +0900

    TienNguyen: add RBM codes

--- src/rbm/rbm.py
+++ src/rbm/rbm.py
@@ -17,20 +17,22 @@ class RBM(object):
        normal distribution with mean 0 and variance 1 and 
        shape = (number_visible(row), number_hidden(column))
        """
-       self.W = torch.randn(number_visible, number_hidden)
+       self.W = torch.randn(number_hidden, number_visible)
        self.a = torch.randn(1, number_hidden) #bias of hidden nodes
        self.b = torch.randn(1, number_visible) #bias of visible nodes
 
     def sample_hidden(self, visible_output):
         wx = torch.mm(visible_output, self.W.t()) 
         activation = wx + self.a.expand_as(wx)
+        import pdb
+        #pdb.set_trace()
         prob_h_given_v_vector = torch.sigmoid(activation)
         #prob_hidden_given_visible is one-hot vector
         prob_h_given_v = torch.bernoulli(prob_h_given_v_vector)
-        return prob_hidden_given_visible
+        return prob_h_given_v_vector, prob_h_given_v
 
     def sample_visible(self, hidden_output):
-        wy = torch.mm(y, self.W)
+        wy = torch.mm(hidden_output, self.W)
         activation = wy + self.b.expand_as(wy)
         prob_v_given_h_vector = torch.sigmoid(activation)
         prob_v_given_h = torch.bernoulli(prob_v_given_h_vector)
@@ -39,4 +41,4 @@ class RBM(object):
     def train(self, v0, vk, ph0, phk):
         self.W += torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)
         self.b += torch.sum((v0 - vk), 0)
-        self.a += torch.sum((p0 - phk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
